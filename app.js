9b38e70923e8d2fad0a14159fdaa6efdef0c6ce6
Diego Fernando <107154026+idegocode@users.noreply.github.com>
2024-04-01 10:54:58 -0500

    primer commit
    inicio del proyecto

---
+++ app.js
@@ -0,0 +1,8 @@
+let numeroSecreto = 6
+let numeroUsuario = prompt("Ingresa un numero por favor:");
+console.log('su numero es',numeroUsuario);
+
+if (numeroUsuario == numeroSecreto)
+{
+    alert('Acertaste el numero')
+}
